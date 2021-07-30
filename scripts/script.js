ce07f9e23e01709b6e5b5f1c3e2e87d6db190b40
jcepedaf <jcepedaf@gmail.com>
2021-07-30 22:16:23 +0200

    agrego ejercicio 2 de inclass

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,14 @@ Look for the files index.html and script.js
 Open index.html with the Visual Studio Live Server and see how the alert appears
 Write a name in the input field Something to do and click the Create button. Right know you don't have to understand the code, but what do you think the code is doing?
 */
+
+/*
+Exercise 2:
+Delete all the existing code in script.js in the folder scripts
+Write code to access the Create button element using document.querySelector()
+Log this element using console.log()
+
+let buttonElement = document.querySelector(".button");
+console.log(buttonElement);*/
+
+
