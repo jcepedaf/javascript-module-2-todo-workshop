520afa8889e0d614c94c4aed06b373fca3112ac0
jcepedaf <jcepedaf@gmail.com>
2021-07-30 22:06:51 +0200

    agrego ejercicio 1 de inclass

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,14 @@
-alert("hello!");
+//alert("hello!");
 
-document.querySelector('#new-todo').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text = e.target.elements.text.value.trim();
-    alert(text);
-})
+//document.querySelector('#new-todo').addEventListener('submit', (e) => {
+//    e.preventDefault();
+//    const text = e.target.elements.text.value.trim();
+//    alert(text);
+//})
+
+/*
+Exercise 1:
+Look for the files index.html and script.js
+Open index.html with the Visual Studio Live Server and see how the alert appears
+Write a name in the input field Something to do and click the Create button. Right know you don't have to understand the code, but what do you think the code is doing?
+*/
